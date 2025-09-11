c129067e4e91d2209e60a10b5f2cfc144f8e68dc
MonseMc20 <monsemc20114@gmail.com>
2025-09-11 13:18:22 -0600

    Added function body to prefixFunction

--- kmpAlgorithm.h
+++ kmpAlgorithm.h
@@ -3,7 +3,23 @@
 #include <vector>
 
 
-std::vector <int>  prefixFunction(const std::string &pattern);
+inline std::vector <int>  prefixFunction(const std::string &pattern){
+    int m = pattern.length();
+    std::vector<int> prefix(m, 0);
+    int k = 0; // longitud del prefijo más largo
+
+    for (int q = 1; q < m; q++) {
+        while (k > 0 && pattern[k] != pattern[q]) {
+            k = prefix[k - 1]; // retrocede en el prefijo
+        }
+        if (pattern[k] == pattern[q]) {
+            k++;
+        }
+        prefix[q] = k;
+    }
+    return prefix;
+
+}
 
 bool KMPSearch(const std::string &text, const std::string &pattern);
 
